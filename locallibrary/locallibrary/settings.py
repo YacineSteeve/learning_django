b3d0ff2112cec964908d1c3171aa793bfe620796
YacineSteeve <steeveboukari9@gmail.com>
2022-05-19 15:55:03 +0200

    Add tests for the book renew form, for the model Author and for the author list view.

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-ivrrmodhg%s6^i!o6+8e2+1)_w2_2#s88_0-xcoaa+dm-pg#$&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
